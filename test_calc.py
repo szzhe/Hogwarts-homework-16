02fadd6b68a99ba0ee3b4d1418a3d41b34c8ff14
songzhenzhe <97228571@qq.com>
2020-12-10 11:01:46 +0800

    first commit

---
+++ test_calc.py
@@ -0,0 +1,31 @@
+from pythoncode.calculator import Calculator
+import pytest
+
+
+class TestCale():
+    def setup_class(self):
+        self.cal = Calculator()
+        print("开始计算")
+
+    def teardown_class(self):
+        print("结束计算")
+
+    @pytest.mark.parametrize("a,b,expect", [
+        # (3, 5, -2),(-1, -2, 1),(100, 300, -200),
+        (3, 5, 8), (-1, -2, -3), (100, 300, 400),
+    ], ids=["int", "minus", "bigint"])
+    def test_add(self, a, b, expect):
+        assert expect == self.cal.add(a, b)
+
+    def test_sub(self, a, b, expect):
+        assert expect == self.cal.sub(a, b)
+
+    def test_mul(self, a, b, expect):
+        assert expect == self.cal.mul(a, b)
+
+    def test_div(self, a, b, expect):
+        assert expect == self.cal.div(a, b)
+
+
+if __name__ == '__main__':
+    pytest.main(["test_calc.py"])
---
+++ test_dome1.py
@@ -0,0 +1,23 @@
+import pytest
+
+
+def add_function(a, b):
+    return a + b
+
+
+@pytest.mark.parametrize("a,b,expected", [
+    (3, 5, 8), (-1, -2, -3), (1000, 1000, 2000)
+], ids=["int", "minus", "bigint"])
+def test_add(a, b, expected):
+    assert add_function(a, b) == expected
+
+
+@pytest.mark.parametrize("a", [0, 1, 5])
+@pytest.mark.parametrize("b", [2, 3, 8])
+def test_add1(a, b):
+    print("测试数据组合：\
+    a->%s, b->%s" % (a, b))
+
+
+if __name__ == '__main__':
+    pytest.main(['-sq', 'test_dome.py'])
