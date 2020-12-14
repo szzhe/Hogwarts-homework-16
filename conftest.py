6b61f05fbe6a501b260496da477334da90c46e0f
songzhenzhe <97228571@qq.com>
2020-12-14 17:08:36 +0800

    conftest calculator

--- conftest.py
+++ conftest.py
@@ -2,7 +2,7 @@ import pytest
 from pythoncode.calculator import Calculator
 
 
-@pytest.fixture(scope="class")
+@pytest.fixture(scope="module")
 def test_calc():
     print("开始计算")
     cal = Calculator()
