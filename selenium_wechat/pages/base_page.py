bbf0c308a1ba7e85827a238c904e7686332db1df
songzhenzhe <97228571@qq.com>
2021-01-05 19:20:23 +0800

    优化代码

--- selenium_wechat/pages/base_page.py
+++ selenium_wechat/pages/base_page.py
@@ -7,27 +7,27 @@ class BasePage:
     def __init__(self, base_driver: WebDriver = None):
         self.driver = base_driver
 
-    def find(self, by, locator=None):
-        if locator is None:
+    def find(self, by, locator: str = None):
+        if isinstance(locator, tuple):
             return self.driver.find_element(*by)
         else:
             return self.driver.find_element(by=by, value=locator)
 
-    def find_click(self, by, locator=None):
-        if locator is None:
+    def find_click(self, by, locator: str = None):
+        if isinstance(locator, tuple):
             self.find(*by).click()
         else:
             self.find(by=by, locator=locator).click()
 
-    def finds(self, by, locator=None):
-        if locator is None:  # <class 'str'>
+    def finds(self, by, locator: str = None):
+        if isinstance(locator, tuple):  # <class 'str'>
             # 如果传入的是一个元祖，则进行解包元祖传参
             return self.driver.find_elements(*by)
         else:
             # 如果传入的是正常的定位信息，则正常传参
             return self.driver.find_elements(by=by, value=locator)
 
-    def find_sendkeys(self, by, locator, text):
+    def find_sendkeys(self, by, locator, text: str):
         self.find(by, locator).send_keys(f"{text}")
 
     def wait_locate(self, by, locator):
--- selenium_wechat/pages/main_page.py
+++ selenium_wechat/pages/main_page.py
@@ -6,18 +6,10 @@ from selenium_wechat.pages.add_member_page import AddMember
 class MainPage(BasePage):
 
     def goto_add_member(self):
-        '''
-        快捷添加通讯录成员
-        :return:
-        '''
         self.find_click(By.CSS_SELECTOR, ".ww_indexImg_AddMember")
         return AddMember(self.driver)
 
     def goto_contact(self):
-        '''
-        跳转到通讯录
-        :return:
-        '''
         self.find_click(By.ID, "menu_contacts")
         return ContactPage(self.driver)
 
